b55d87bac5ed6c60c17d3455005bd97676478785
pedrohenrique0134 <73433708+pedrohenrique0134@users.noreply.github.com>
2024-07-30 01:54:57 -0300

    - Entrada de dados na main. - modifiquei o xml principal.

--- app/src/main/java/com/app/converttemperature/MainActivity.kt
+++ app/src/main/java/com/app/converttemperature/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.app.converttemperature
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +14,15 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val button: LinearLayout = findViewById(R.id.btn_conv)
+
+        button.setOnClickListener {
+            val temperatura: EditText = findViewById(R.id.temperatura)
+            var transforme = (temperatura.text.toString().toInt() * 1.8) + 32
+            val resultado: TextView = findViewById(R.id.resultado)
+            resultado.visibility = View.VISIBLE
+            resultado.setText(transforme.toString())
         }
     }
 }
